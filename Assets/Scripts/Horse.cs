9c805fbf7249c1d1509ddac62a32819ea2e53502
NBtheMC <nvbhusha@ucsc.edu>
2021-11-29 17:20:08 -0800

    BoomerangHorse started

--- Assets/Scripts/Horse.cs
+++ Assets/Scripts/Horse.cs
@@ -6,34 +6,87 @@ using UnityEngine;
 public class Horse : MonoBehaviour
 {
     private GameObject currentCharacter; //character that can ride horse
-    private bool beingRidden;
+    private bool isCharging;
+    private bool isShooting;
+    public float currentAmmo;
+    private float chargeTime;
+    private float shootPower;
     // Start is called before the first frame update
     void Start()
     {
-        //only this cowboy can ride this horse. they're best friends and can never be seperated
-        currentCharacter = this.transform.parent.gameObject;
+        isCharging = false;
+        isShooting = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //MOUNTING
-            if(Input.GetKeyDown("enter")){
-            float distance = Vector3.Distance(this.transform.position, currentCharacter.transform.position);
-                //cowboy has to be right next to horse to mount
-                if(distance < 0.5f && !beingRidden){
-                    Vector3 seatedPosition = new Vector3(currentCharacter.transform.position.x, currentCharacter.transform.position.y + 0.5f, 0);
-                    currentCharacter.transform.position = seatedPosition;
-                    currentCharacter.transform.parent = this.transform;
-                    beingRidden = true;
-                }
-                //detach cowboy from horse
-                else if(beingRidden){
-                    currentCharacter.transform.parent = null;
-                    beingRidden = false;
-                }
-        }
+        // //MOUNTING
+        //     if(Input.GetKeyDown("enter")){
+        //     float distance = Vector3.Distance(this.transform.position, currentCharacter.transform.position);
+        //         //cowboy has to be right next to horse to mount
+        //         if(distance < 0.5f && !beingRidden){
+        //             Vector3 seatedPosition = new Vector3(currentCharacter.transform.position.x, currentCharacter.transform.position.y + 0.5f, 0);
+        //             currentCharacter.transform.position = seatedPosition;
+        //             currentCharacter.transform.parent = this.transform;
+        //             beingRidden = true;
+        //         }
+        //         //detach cowboy from horse
+        //         else if(beingRidden){
+        //             currentCharacter.transform.parent = null;
+        //             beingRidden = false;
+        //         }
+        // }
         //RIDING
         //based on player movement, so implement later
+
+        //SHOOTING 
+        // pressing enter to shoot
+        if (Input.GetKeyDown(KeyCode.Return) && currentAmmo != 0){
+            Debug.Log("Start");
+            isCharging = true;
+        }
+        //charging up
+        else if(isCharging && chargeTime < 3f && Input.GetKey(KeyCode.Return)){
+            Debug.Log("Charging");
+            chargeTime += Time.deltaTime;
+        }
+        //actually shoot
+        else if(isCharging && (chargeTime >= 3f || Input.GetKeyUp(KeyCode.Return))){
+            Debug.Log("Shooting");
+            currentAmmo -= 0;
+            this.transform.parent = null;
+            float power = chargeTime/10;
+            StartCoroutine("BoomerangHorse", power);
+        }
+    }
+
+    private IEnumerator BoomerangHorse(float power){
+        if(this.isShooting){
+            yield break;
+        }
+        //Lose horse
+        isShooting = true;
+        currentAmmo =0;
+        float timer = power * 10;
+        Debug.Log(timer);
+        float timePassed = 0;
+
+        Transform thisHorse = this.GetComponent<Transform>();
+
+        //move and spin in certain direction
+        while(timePassed <= timer/2){
+            timePassed += Time.deltaTime;
+            thisHorse.position = thisHorse.position + new Vector3(1f, 1f, 0f);
+        }
+        //move back towards player AND SPIN
+        timePassed = 0;
+        while(timePassed <= timer/2){
+            timePassed += Time.deltaTime;
+            thisHorse.position = thisHorse.position - new Vector3(1f, 1f, 0f);
+        }
+
+        //Gain horse back and horse disappears?
+        currentAmmo = 1;
     }
 }
